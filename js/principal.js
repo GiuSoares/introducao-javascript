8ae66d21bbef55d751452ea786540fbe83a1c06c
Giulia <giuliasoaresrs@gmail.com>
2022-09-12 23:50:15 -0300

    Iniciando projeto
    Iniciando projeto

---
+++ js/principal.js
@@ -0,0 +1,50 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Giulia Nutrição" // manipulando o título via JS
+//alert("Giulia é muito gata!"); //alertinha da massa
+console.log(titulo);
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var pacientePeso = paciente.querySelector(".info-peso");
+var peso = pacientePeso.textContent;
+
+
+var pacienteAltura = paciente.querySelector(".info-altura");
+var altura = pacienteAltura.textContent;
+
+var tdImc = document.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValida= true;
+
+if (peso <= 0 || peso >= 1000){
+    console.log("Peso Inválido");
+    pesoValido = false;
+    tdImc.textContent = "Peso Inválido";
+}
+
+if (altura<0 ||altura >3.00){
+    console.log("Altura Inválida");
+    alturaValida = false;
+    tdImc.textContent = "Altura Inválida";
+   }
+
+
+if (alturaValida && pesoValido){
+    var imc = peso/(altura*altura);
+    tdImc.textContent=imc;
+}
+
+
+console.log(pacientePeso);
+console.log(pacienteAltura);
+
+
+
+
+
+
+
+
+
+		
