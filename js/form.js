48c9ea9afbc3b782178332c495cd3f0a66f47158
Giulia <giuliasoaresrs@gmail.com>
2022-09-13 22:22:50 -0300

    Separando funções em arquivos

--- js/form.js
+++ js/form.js
@@ -1,49 +1,3 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Giulia Nutrição" // manipulando o título via JS
-//alert("Giulia é muito gata!"); //alertinha da massa
-console.log(titulo);
-
-
-var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes)
-
-for(var i= 0; i < pacientes.length; i++){
-    var paciente = pacientes[i];
-    var pacientePeso = paciente.querySelector(".info-peso");
-    var peso = pacientePeso.textContent;
-    var pacienteAltura = paciente.querySelector(".info-altura");
-    var altura = pacienteAltura.textContent;
-    var tdImc = paciente.querySelector(".info-imc");
-    var pesoValido = true;
-    var alturaValida= true;
-
-
-    if (peso <= 0 || peso >= 1000){
-    console.log("Peso Inválido");
-    pesoValido = false;
-    tdImc.textContent = "Peso Inválido";
-    paciente.classList.add("paciente-invalido");
-    }
-
-    if (altura<0 ||altura >3.00){
-    console.log("Altura Inválida");
-    alturaValida = false;
-    tdImc.textContent = "Altura Inválida";
-    paciente.classList.add("paciente-invalido");
-    }
-
-
-    if (alturaValida && pesoValido){
-    var imc = peso/(altura*altura);
-    tdImc.textContent=imc.toFixed(2);
-    }
-}
-
-titulo.addEventListener("click",function(){
-    console.log("Olá, eu fui clicado!");
-});
-
-
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault(); //evita que o default do clique de botão seja feito (submeter form e recarregar página)
@@ -90,15 +44,3 @@ botaoAdicionar.addEventListener("click", function(event){
 
     
 })
-
-
-/*function mostraMensagem(){
-  console.log("Olá, eu fui clicado!");
-  alert("Fui clicado"); 
-}*/
-
-
-
-
-
-		
